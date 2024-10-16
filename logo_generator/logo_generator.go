e355dfa15ea627028b263fb35ab4ace363ef2a72
liubavabarsegian <lyubahemmo@mail.ru>
2024-10-17 00:06:14 +0300

    Logo generator

---
+++ logo_generator/logo_generator.go
@@ -0,0 +1,42 @@
+package logo_generator
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+type Pixel struct {
+	X     int
+	Y     int
+	Color color.RGBA
+}
+
+func Generate(imageName string) {
+	imageMatrix := selectImage(imageName)
+
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+
+	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+	for _, pixel := range imageMatrix {
+		for dx := 0; dx < PixelSize; dx++ {
+			for dy := 0; dy < PixelSize; dy++ {
+				img.Set(pixel.Y*PixelSize+dy, pixel.X*PixelSize+dx, pixel.Color)
+			}
+		}
+	}
+
+	f, _ := os.Create("amazing_logo.png")
+	png.Encode(f, img)
+}
+
+func selectImage(imageName string) []Pixel {
+	switch imageName {
+	case "frog":
+		return FrogImagePixels
+	}
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"day06/logo_generator"
+)
+
+func main() {
+	logo_generator.Generate("frog")
+}
